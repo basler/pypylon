2c77eb15661e94ddd7fa28b39755a3ab3bb7d2d0
Farzad Ahmadinejad <farzadsw@gmail.com>
2018-07-14 13:19:06 +0430

    Adding opencv example

---
+++ samples/opencv.py
@@ -0,0 +1,37 @@
+'''
+A simple Program for grabing video from basler camera and converting it to opencv img.
+Tested on Basler acA1300-200uc (USB3, linux 64bit , python 3.5)
+
+'''
+from pypylon import pylon
+import cv2
+
+# conecting to the first available camera
+camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+
+# Grabing Continusely (video) with minimal delay
+camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly) 
+converter = pylon.ImageFormatConverter()
+
+# converting to opencv bgr format
+converter.OutputPixelFormat = pylon.PixelType_BGR8packed
+converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
+
+while camera.IsGrabbing():
+    grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+
+    if grabResult.GrabSucceeded():
+        # Access the image data
+        image = converter.Convert(grabResult)
+        img = image.GetArray()
+        cv2.namedWindow('title', cv2.WINDOW_NORMAL)
+        cv2.imshow('title', img)
+        k = cv2.waitKey(1)
+        if k == 27:
+            break
+    grabResult.Release()
+    
+# Releasing the resource    
+camera.StopGrabbing()
+
+cv2.destroyAllWindows()
