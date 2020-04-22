b2646bfb2b54efe62918dd4d9712d5890a92e40d
Stefan Klug <stefan.klug@baslerweb.com>
2020-04-22 14:49:07 +0200

    Add -DSWIGWORDSIZE
    This is required in the linux/os x build and went unnoticed for a long time.
    It lead to intptr_t types that were truncated to 32bit.
    One result was that callbacks where not deregistered successfully.

--- setup.py
+++ setup.py
@@ -606,13 +606,15 @@ class BuildSupportLinux(BuildSupport):
     def __init__(self):
         super(BuildSupportLinux, self).__init__()
         self.SwigExe = self.find_swig()
+
+        self.SwigOptions.append("-DSWIGWORDSIZE%i" % (get_machinewidth(),) )
+
         config_cflags = self.call_pylon_config("--cflags")
         self.ExtraCompileArgs.extend(config_cflags.split())
         print("ExtraCompileArgs:", self.ExtraCompileArgs)
         config_libs = self.call_pylon_config("--libs")
         self.ExtraLinkArgs.extend(config_libs.split())
         print("ExtraLinkArgs:", self.ExtraLinkArgs)
-
 
         config_libdir = self.call_pylon_config("--libdir")
         self.LibraryDirs.extend(config_libdir.split())
@@ -712,6 +714,9 @@ class BuildSupportMacOS(BuildSupport):
     def __init__(self):
         super(BuildSupportMacOS, self).__init__()
         self.SwigExe = self.find_swig()
+
+        self.SwigOptions.append("-DSWIGWORDSIZE%i" % (get_machinewidth(),) )
+
         includes_dir = os.path.abspath(
             os.path.join('.' , "osx_includes")
             )
