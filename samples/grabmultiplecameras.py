c849b1d27608fcf72b2a9439d340094809730126
BahadirGLCK <bahadirgolcuk@gmail.com>
2020-05-08 16:13:58 +0300

    Changed RUNTIME_EXCEPTION in /samples/grap.py and rebuild

--- samples/grabmultiplecameras.py
+++ samples/grabmultiplecameras.py
@@ -43,10 +43,11 @@ try:
     tlFactory = pylon.TlFactory.GetInstance()
 
     # Get all attached devices and exit application if no device is found.
-    devices = tlFactory.EnumerateDevices()
+    #devices = tlFactory.EnumerateDevices()
+    devices = list()
     if len(devices) == 0:
-        raise pylon.RuntimeException("No camera present")
-
+        #raise pylon.RuntimeException("No camera present")
+        raise pylon.RUNTIME_EXCEPTION("No camera present")
     # Create an array of instant cameras for the found devices and avoid exceeding a maximum number of devices.
     cameras = pylon.InstantCameraArray(min(len(devices), maxCamerasToUse))
 
