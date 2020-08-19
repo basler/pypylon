3626d942ecb17ef6a824a7c177b5607928dea4ff
Stefan Klug <stefan.klug@baslerweb.com>
2020-08-19 09:16:20 +0200

    Updated to newest pylon on windows

--- VersionInfo.py
+++ VersionInfo.py
@@ -1,6 +1,6 @@
 # The pylon version this source tree was designed for, by platform
 ReferencePylonVersion = {
-    "Windows": "6.1.0",
+    "Windows": "6.1.1",
     # ATTENTION: This version is the pylon core version reported by pylon-config, 
     # which is not equal to the version on the outer tar.gz
     "Linux": "6.1.0",
