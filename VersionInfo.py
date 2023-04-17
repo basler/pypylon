b172bea4b77da7941a4a138d856fabd8c730fa4f
Stefan Klug <stefan.klug@baslerweb.com>
2023-04-17 15:56:13 +0200

    Update pylon reference version

--- VersionInfo.py
+++ VersionInfo.py
@@ -1,9 +1,9 @@
 # The pylon version this source tree was designed for, by platform
 ReferencePylonVersion = {
-    "Windows": "6.3.0",
+    "Windows": "7.2.1",
     # ATTENTION: This version is the pylon core version reported by pylon-config,
     # which is not equal to the version on the outer tar.gz
-    "Linux": "6.2.0",
-    "Linux_x86_64": "6.3.0",
+    "Linux": "7.2.1",
+    "Linux_armv7l": "6.2.0",
     "Darwin": "6.1.2"
 }
